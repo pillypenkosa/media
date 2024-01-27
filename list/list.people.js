a58043aee760132fdc405b37416248678a2e17bf
pillypenkosa <pillypenkosa@gmail.com>
2024-01-27 16:33:27 +0200

    Media 2024-01-27 16:00

--- list/list.people.js
+++ list/list.people.js
@@ -18006,6 +18006,15 @@ let arrListPeople = [
 	internet: { wiki_ua: '%D0%A0%D1%83%D1%83%D0%B4_%D0%93%D1%83%D0%BB%D0%BB%D1%96%D1%82', },
 },
 {
+	id: 'ryan_reynolds_23101976', 
+	name: { n: 'Ryan', s: 'Reynolds', },
+	sex: 1,
+	life: { bd: 23, bm: 10, by: 1976, },
+	country: { can: 1, usa: 1, },
+	hash: { movie: 1, actor: 1, },
+	internet: { imdb: '0005351', wiki_ua: '%D0%A0%D0%B0%D1%8F%D0%BD_%D0%A0%D0%B5%D0%B9%D0%BD%D0%BE%D0%BB%D1%8C%D0%B4%D1%81', },
+},
+{
 	id: 'saddam_hussein_28041937', 
 	name: { n: 'Saddam', s: 'Hussein', },
 	sex: 1,
