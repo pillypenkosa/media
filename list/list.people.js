1ac9dbb5e3d5c0ad2f3d5d2adfd083ba1978f669
pillypenkosa <pillypenkosa@gmail.com>
2024-03-27 13:05:48 +0200

    Media 2024-03-27 12:05

--- list/list.people.js
+++ list/list.people.js
@@ -6772,6 +6772,15 @@ let arrListPeople = [
 	internet: { wiki_ua: '%D0%AF%D1%80%D0%BE%D1%81%D0%BB%D0%B0%D0%B2_%D0%9C%D1%83%D0%B4%D1%80%D0%B8%D0%B9', },
 },
 {
+	id: 'myhaylo_petro_mudryk_05012001', 
+	name: { n: 'Михайло', s: 'Мудрик', p: 'Петрович', },
+	sex: 1,
+	life: { bd: 5, bm: 1, by: 2001,  },
+	country: { ukr: 1, },
+	hash: { sport: 1, football: 1, },
+	internet: { wiki_ua: '%D0%9C%D1%83%D0%B4%D1%80%D0%B8%D0%BA_%D0%9C%D0%B8%D1%85%D0%B0%D0%B9%D0%BB%D0%BE_%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D0%B2%D0%B8%D1%87', },
+},
+{
 	id: 'irina_muravyova_08021949', 
 	name: { n: 'Ирина', s: 'Муравьёва', },
 	life: { bd: 8, bm: 2, by: 1949, },
