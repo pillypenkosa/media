9bc9f44dec9da360936ab29a5f84e5e727fdcea4
pillypenkosa <pillypenkosa@gmail.com>
2024-11-22 10:22:55 +0200

    Media 2024-11-22 10:15

--- list/list.people.js
+++ list/list.people.js
@@ -11507,18 +11507,14 @@ let arrListPeople = [
 	internet: { wiki_ua: '%D0%AF%D1%80%D0%BE%D1%88_%D0%94%D0%BC%D0%B8%D1%82%D1%80%D0%BE_%D0%90%D0%BD%D0%B0%D1%82%D0%BE%D0%BB%D1%96%D0%B9%D0%BE%D0%B2%D0%B8%D1%87', },
 },
 {
-	id: 'igor_yasulovich_24091941', 
-	name: { n: 'Игорь', s: 'Ясулович', },
+	id: 'igor_mykola_yasulovich_24091941', 
+	name: { n: 'Ігор', s: 'Ясулович', p: 'Миколайович', },
 	sex: 1,
 	life: { bd: 24, bm: 9, by: 1941, },
 	img: [ 1, 1 ],
-	country: { rus: 1, our: { sun: 1, }, },
-	movie: { actor: {}, },
-	internet: {
-		imdb: '0946670', 
-		wiki_ru: '%D0%AF%D1%81%D1%83%D0%BB%D0%BE%D0%B2%D0%B8%D1%87,_%D0%98%D0%B3%D0%BE%D1%80%D1%8C_%D0%9D%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B5%D0%B2%D0%B8%D1%87', 
-		kinoteatr: 'https://www.kino-teatr.ru/kino/acter/m/ros/5096/works/', 
-	},
+	country: { sun: 1, rus: 1, },
+	hash: { movie: 1, actor: 1, },
+	internet: { imdb: '0946670', wiki_ua: '%D0%AF%D1%81%D1%83%D0%BB%D0%BE%D0%B2%D0%B8%D1%87_%D0%86%D0%B3%D0%BE%D1%80_%D0%9C%D0%B8%D0%BA%D0%BE%D0%BB%D0%B0%D0%B9%D0%BE%D0%B2%D0%B8%D1%87', },
 },
 {
 	id: 'arseny_petro_yacenyuk_22051974', 
